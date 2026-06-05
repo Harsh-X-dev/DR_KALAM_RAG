b7e65c93bc804a1bbc0f204fe5aad50fb8ba1fa8
Harsh-X-dev <harshsainib@gmail.com>
2026-06-05 22:43:27 +0530

    build repaired

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import typography from "@tailwindcss/typography";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), typography],
+  plugins: [react(), tailwindcss()],
 });
